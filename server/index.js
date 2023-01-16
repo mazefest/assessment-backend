b2a9cb02b273828a661cb5aa7c603ef1490f6bcc
Mazefest <colbymehmen@gmail.com>
2023-01-16 09:48:00 -0600

    added routes

--- server/index.js
+++ server/index.js
@@ -7,9 +7,13 @@ app.use(cors());
 
 app.use(express.json());
 
-const { getCompliment, getFortune } = require('./controller')
+const { getCompliment, getFortune , getCompliments, deleteComplimentWithID, addCompliment } = require('./controller')
 
 app.get("/api/compliment", getCompliment);
 app.get("/api/fortune", getFortune);
+
+app.get("/api/compliments", getCompliments);
+app.post("/api/compliments", addCompliment);
+app.delete("/api/compliments/:id", deleteComplimentWithID);
 
 app.listen(4000, () => console.log("Server running on 4000"));
